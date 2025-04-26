59481f5d3fdbedb44163a3a439ff290e843ec591
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-04-26 22:58:54 +0800

    fix: 修复包名

--- scripts/install_webvirt_cloud_retspen.sh
+++ scripts/install_webvirt_cloud_retspen.sh
@@ -135,7 +135,7 @@ install_dependencies() {
         "git" "virtualenv" "python3-virtualenv" "supervisor"
         "libsasl2-modules" "wget" "curl" "nginx"
         "qemu-kvm" "libvirt-daemon-system" "libvirt-clients" "bridge-utils" "virt-manager" "sasl2-bin"
-        "libldap2-dev" "libsasl2-dev" "lsb_release"
+        "libldap2-dev" "libsasl2-dev" "lsb-release"
     )
     for pkg in "${packages[@]}"; do
         _blue "正在安装: $pkg"
